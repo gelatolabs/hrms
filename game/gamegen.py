d7ce88adaa8ec997ef0618d5ef92eeb9fa572053
Matthew Petry <firetwoonenine@gmail.com>
2020-10-02 23:54:28 -0500

    baby's first fake identity generator

--- game/gamegen.py
+++ game/gamegen.py
@@ -1,4 +1,5 @@
 import json
+from random import randint
 
 # Backend Game Functions for:
 #
@@ -9,3 +10,34 @@ import json
 # Makes use of pdfTeX by The pdfTeX Team (Hàn Thế Thành et al), licensed under GPL (see LICENSE.pdftex file)
 #
 # All original code under the ISC license (see COPYING for details)
+
+def getNoun():
+  nounnum = randint(1, 6797)
+  with open("datasets/nouns.txt") as nounfile:
+    for i, line in enumerate(nounfile):
+        if i == nounnum:
+            return line.strip("\n")
+
+
+def generateName():
+  fnamenum = randint(1, 2718)
+  with open("datasets/f_names.txt") as fnamefile:
+    for i, line in enumerate(fnamefile):
+        if i == fnamenum:
+            fname = line.strip("\n")
+  lname = getNoun()
+
+  return (fname + " " + lname)
+
+
+def generateAddress():
+  roadtypes = ["Lane","Road","Circle","Blvd","Street"]
+  roadtype = roadtypes[randint(0,4)]
+  housenumber = randint(1,999)
+  roadname = getNoun()
+  return (str(housenumber) + " " + roadname + " " + roadtype)
+  
+  
+
+print(generateName())
+print(generateAddress())
